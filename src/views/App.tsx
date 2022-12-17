88358ea7fd0a68fad7eca3ca710b23c6d412cbe8
Michael Tedder <mtedder@coldfusion.co.jp>
2022-12-17 19:44:58 +0900

    simple control over cube spin rate using the mouse

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -12,17 +12,36 @@ const geometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
 const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 
+let clicked = false;
+let spinX = 0.0, spinY = 0.0;
+let lastPos = null;
+
 function createScene(scene) {
 	scene.add(cube);
 }
 
 function updateScene(scene, time) {
-	cube.rotation.x = time / 2000.0;
-	cube.rotation.y = time / 1000.0;
+	// auto animate if not yet controlled using the mouse
+	if (!clicked) {
+		cube.rotation.x = time / 2000.0;
+		cube.rotation.y = time / 1000.0;
+	} else {
+		cube.rotation.y += spinX;
+		cube.rotation.x += spinY;
+	}
 }
 
 function mouseEvent(state) {
-	console.log('pos: (' + state.pos.x + ', ' + state.pos.y + '), buttons: ' + state.buttons);
+	// calculate relative movement from last mouse position
+	const last = lastPos ?? state.pos;
+	const rel = { x: state.pos.x - last.x, y: state.pos.y - last.y };
+	lastPos = state.pos;
+
+	if (state.buttons & 1) {
+		spinX = rel.x * 1.5;
+		spinY = rel.y * 1.5;
+		clicked = true;
+	}
 }
 
 const App = ({ userContext, environment }: ExtensionContextValue) => {
